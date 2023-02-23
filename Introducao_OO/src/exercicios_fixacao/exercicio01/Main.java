90c5a4febb04706607f54189aa087648f5661dc4
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2023-02-22 22:59:48 -0300

    atualizacao exercicio01

--- Introducao_OO/src/exercicios_fixacao/exercicio01/Main.java
+++ Introducao_OO/src/exercicios_fixacao/exercicio01/Main.java
@@ -1,9 +1,23 @@
 package exercicios_fixacao.exercicio01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Retangulo retang = new Retangulo();
+		
+		System.out.println("Insira, respectivamente, a altura e largura do retangulo:");
+		retang.altura = sc.nextDouble();
+		retang.largura = sc.nextDouble();
+		
+		sc.close();
+		
+		System.out.println(retang);
 
 	}
 
--- Introducao_OO/src/exercicios_fixacao/exercicio01/Retangulo.java
+++ Introducao_OO/src/exercicios_fixacao/exercicio01/Retangulo.java
@@ -1,5 +1,23 @@
 package exercicios_fixacao.exercicio01;
 
 public class Retangulo {
-
+	public double altura;
+	public double largura;
+	
+	public double area() {
+		return altura * largura;
+	}
+	
+	public double perimetro() {
+		return (altura + largura) * 2.0;
+	}
+	
+	public double diagonal() {
+		return Math.sqrt(Math.pow(altura, 2) + Math.pow(largura, 2));
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("AREA = %.2f \nPERIMETRO = %.2f \nDIAGONAL = %.2f", area(), perimetro(), diagonal());
+	}
 }
